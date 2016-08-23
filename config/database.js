abd04bd39deb09c2575470188e384e7a8b736ffa
Rustam Alashrafov <rustam.alashrafov@gmail.com>
2016-08-24 00:12:35 +0200

    Updated database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // Here we define the variables required for the current configuration.
 module.exports = {
     cassandra: {
-        url: 'ec2-52-31-85-51.eu-west-1.compute.amazonaws.com',
+        url: 'ec2-52-209-210-139.eu-west-1.compute.amazonaws.com',
         keyspace: 'test'
     }
 }
